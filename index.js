5bb23cb2b95dd347328fa4d35d3411590b4cd96c
shuva9800 <ffshuva@gmail.com>
2024-06-05 09:33:47 +0530

    socket.io

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express= require('express');
 const {dbconnect} = require("./config/database");
 require("dotenv").config();
+const http = require('http');
 const route = require('./routs/userrouts');
 const cors = require('cors');
+const socketIo = require('socket.io');
 
 const app = express();
 const port= process.env.PORT ||4000;
+
+//socket.io connetc
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors:{
+        origin: "http://localhost:5173/",
+        methods : ['GET', 'POST']
+    }
+});
 
 //body parser 
 app.use(express.json());
@@ -16,12 +27,33 @@ app.use(cors());
 //dbconnect
 dbconnect();
 
-app.listen(port, ()=>{
-    console.log(`app started at port ${port}`);
-})
+
 
 //routing
 app.use("/api/v1", route);
+//iowala
+
+const liveUsers = new Map();
+
+io.on('connection', (socket) => {
+    console.log('New client connected:', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected:', socket.id);
+        liveUsers.delete(socket.id);
+        io.to('liveUsers').emit('updateUsers', Array.from(liveUsers.values()));
+    });
+
+    socket.on('newUser', (user) => {
+        liveUsers.set(socket.id, { name: user.name, email: user.email, socketId: socket.id });
+        socket.join('liveUsers');
+        io.to('liveUsers').emit('updateUsers', Array.from(liveUsers.values()));
+    });
+});
+
+server.listen(port, ()=>{
+    console.log(`app started at port ${port}`);
+})
 
 app.get('/',(req,res)=>{
     res.send(`<h1>successfully exicuted default route ,home page</h1>`)
